df35be61a77a16d844b6fe9db62a7d0a41aaf6cd
Mikael Ahlström <mikael.ahlstrom@metropolia.fi>
2018-03-02 09:59:23 +0200

    cleaned. Crashes when media ends.

--- src/pages/front/front.ts
+++ src/pages/front/front.ts
@@ -54,6 +54,8 @@ export class FrontPage {
 
   ionViewDidLoad() {
 
+    console.log(this.mediaArray);
+
     // Check user
     const userToken = this.mediaProvider.userHasToken();
     if (userToken) {
@@ -66,34 +68,37 @@ export class FrontPage {
   }
 
   loadMedia() {
-    this.mediaProvider.getAllMedia().subscribe(data => {
-      this.mediaArray = data;
-      this.mediaArray.reverse();
-      console.log(this.mediaArray);
-      this.slicedMedia = this.mediaArray.slice(this.picIndex, this.a);
-      this.displayedMedia = this.slicedMedia;
-      this.grid = Array(Math.ceil(this.displayedMedia.length / 2)); //MATHS!
-      let rowNum = 0; //counter to iterate over the rows in the grid
-      console.log("loading media from index " + this.picIndex, "to " + this.loadLimit);
 
-      for (let i = 0; i < this.displayedMedia.length; i += 2) { //iterate images
-        this.grid[rowNum] = Array(2); //declare two elements per row
-        if (this.displayedMedia[i]) { //check file URI exists
-          this.grid[rowNum][0] = this.displayedMedia[i]; //insert image
-        }
-        if (this.displayedMedia[i + 1]) { //repeat for the second image
-          this.grid[rowNum][1] = this.displayedMedia[i + 1];
-        }
-        rowNum++; //go on to the next row
-      }
-      this.picIndex = this.picIndex + 10;
-      this.loadLimit = this.picIndex + 10;
-      console.log("next media will be from index " + this.picIndex, "to " + this.a);
-    });
-  }
+    if(this.mediaArray == undefined /* First load */){
 
-  doInfinite(infiniteScroll) {
-    setTimeout(() => {
+      this.mediaProvider.getAllMedia().subscribe(data => {
+        console.log("First load");
+        this.mediaArray = data;
+        this.mediaArray.reverse();
+        console.log(this.mediaArray);
+        this.slicedMedia = this.mediaArray.slice(this.picIndex, this.loadLimit);
+        this.displayedMedia = this.slicedMedia;
+        this.grid = Array(Math.ceil(this.displayedMedia.length / 2)); //MATHS!
+        let rowNum = 0; //counter to iterate over the rows in the grid
+        console.log("loading media from index " + this.picIndex, "to " + this.loadLimit);
+
+        for (let i = 0; i < this.displayedMedia.length; i += 2) { //iterate images
+          this.grid[rowNum] = Array(2); //declare two elements per row
+          if (this.displayedMedia[i]) { //check file URI exists
+            this.grid[rowNum][0] = this.displayedMedia[i]; //insert image
+          }
+          if (this.displayedMedia[i + 1]) { //repeat for the second image
+            this.grid[rowNum][1] = this.displayedMedia[i + 1];
+          }
+          rowNum++; //go on to the next row
+        }
+        this.picIndex = this.picIndex + 10;
+        this.loadLimit = this.picIndex + 10;
+        console.log("next media will be from index " + this.picIndex, "to " + this.loadLimit);
+      });
+
+    } else /* Infinite Scroll */ {
+      console.log("loadMedia case 2");
       this.displayedMedia = this.displayedMedia.concat(this.mediaArray.slice(this.picIndex, this.loadLimit));
       this.grid = Array(Math.ceil(this.displayedMedia.length / 2)); //MATHS!
       console.log("loading media from index " + this.picIndex, "to " + this.loadLimit);
@@ -112,8 +117,14 @@ export class FrontPage {
       this.picIndex = this.picIndex + 10;
       this.loadLimit = this.picIndex + 10;
       console.log("next media will be from index " + this.picIndex, "to " + this.loadLimit);
+    }
 
+  }
 
+  doInfinite(infiniteScroll) {
+    console.log("infinite scroll");
+    setTimeout(() => {
+      this.loadMedia();
       infiniteScroll.complete();
     }, 500);
   }
