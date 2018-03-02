1c83c7a51b84b5d1fec4c4fb65ed1508af556f39
Mikael Ahlström <mikael.ahlstrom@metropolia.fi>
2018-03-02 09:42:57 +0200

    cleaning

--- src/pages/front/front.ts
+++ src/pages/front/front.ts
@@ -27,7 +27,7 @@ export class FrontPage {
 
   picIndex = 0;
   items = [];
-  a = 10;
+  loadLimit = 10;
   rowNum = 0;
   newestMedia: Array<string>;
 
@@ -37,13 +37,11 @@ export class FrontPage {
   }
 
   doRefresh(refresher) {
-    console.log('Begin async operation', refresher);
-
+    console.log("REFRESH!");
     setTimeout(() => {
       this.picIndex = 0;
-      this.a = 10;
+      this.loadLimit = 10;
       this.loadMedia();
-      console.log('Async operation has ended');
       refresher.complete();
     }, 2000);
   }
@@ -55,6 +53,8 @@ export class FrontPage {
   }
 
   ionViewDidLoad() {
+
+    // Check user
     const userToken = this.mediaProvider.userHasToken();
     if (userToken) {
       this.mediaProvider.getUserData(userToken).subscribe((result: User) => {
@@ -65,72 +65,55 @@ export class FrontPage {
     this.loadMedia();
   }
 
-  loadMedia(){
+  loadMedia() {
     this.mediaProvider.getAllMedia().subscribe(data => {
       this.mediaArray = data;
       this.mediaArray.reverse();
-      console.log("pic index " + this.picIndex, "a " + this.a);
+      console.log(this.mediaArray);
       this.slicedMedia = this.mediaArray.slice(this.picIndex, this.a);
       this.displayedMedia = this.slicedMedia;
-      console.log(this.displayedMedia);
-      //console.log(data);
-      //console.log(this.mediaArray.slice(0, 10));
-      //console.log(this.mediaArray.slice(10, 13));
       this.grid = Array(Math.ceil(this.displayedMedia.length / 2)); //MATHS!
       let rowNum = 0; //counter to iterate over the rows in the grid
+      console.log("loading media from index " + this.picIndex, "to " + this.loadLimit);
 
       for (let i = 0; i < this.displayedMedia.length; i += 2) { //iterate images
-
         this.grid[rowNum] = Array(2); //declare two elements per row
-
         if (this.displayedMedia[i]) { //check file URI exists
           this.grid[rowNum][0] = this.displayedMedia[i]; //insert image
         }
-
         if (this.displayedMedia[i + 1]) { //repeat for the second image
           this.grid[rowNum][1] = this.displayedMedia[i + 1];
         }
-
         rowNum++; //go on to the next row
-
       }
       this.picIndex = this.picIndex + 10;
-      this.a = this.picIndex + 10;
-      console.log("pic index " + this.picIndex, "a " + this.a);
+      this.loadLimit = this.picIndex + 10;
+      console.log("next media will be from index " + this.picIndex, "to " + this.a);
     });
   }
 
   doInfinite(infiniteScroll) {
-    console.log('Begin async operation');
-
     setTimeout(() => {
-      this.displayedMedia = this.displayedMedia.concat(this.mediaArray.slice(this.picIndex, this.a));
-      console.log(this.displayedMedia);
+      this.displayedMedia = this.displayedMedia.concat(this.mediaArray.slice(this.picIndex, this.loadLimit));
       this.grid = Array(Math.ceil(this.displayedMedia.length / 2)); //MATHS!
-      console.log(this.grid);
+      console.log("loading media from index " + this.picIndex, "to " + this.loadLimit);
 
       for (let i = 0; i < this.displayedMedia.length; i += 2) { //iterate images
-
         this.grid[this.rowNum] = Array(2); //declare two elements per row
-
         if (this.displayedMedia[i]) { //check file URI exists
           this.grid[this.rowNum][0] = this.displayedMedia[i]; //insert image
         }
-
         if (this.displayedMedia[i + 1]) { //repeat for the second image
           this.grid[this.rowNum][1] = this.displayedMedia[i + 1];
         }
-
         this.rowNum++; //go on to the next row
       }
-      console.log(this.grid);
-      console.log(this.picIndex);
 
       this.picIndex = this.picIndex + 10;
-      this.a = this.picIndex + 10;
-      console.log("pic index " + this.picIndex, "a " + this.a);
+      this.loadLimit = this.picIndex + 10;
+      console.log("next media will be from index " + this.picIndex, "to " + this.loadLimit);
 
-      console.log('Async operation has ended');
+
       infiniteScroll.complete();
     }, 500);
   }
