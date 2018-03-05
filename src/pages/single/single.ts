0a92d5aeb7c0bb737bc60305b9cdfdd2f0654913
Mikael Ahlström <mikael.ahlstrom@metropolia.fi>
2018-03-05 23:06:39 +0200

    comments

--- src/pages/single/single.ts
+++ src/pages/single/single.ts
@@ -21,7 +21,6 @@ import {Comment} from '../../app/interfaces/comment';
 export class SinglePage {
   url: string;
   title: string;
-  latLon: any;
   comment: string;
   description: string;
 
@@ -39,10 +38,40 @@ export class SinglePage {
   private likesSet: Object;
   private commentsArray: Object;
   private amountOfComments: number;
+  private ownPicArray: any;
+  private ppArray: any;
+  private newestPicIndex: number;
+  private profilePicUrl: string;
+  private commenter: Object = {};
 
   constructor(public navCtrl: NavController, public navParams: NavParams,
               public mediaProvider: MediaProvider, public mapProvider: MapProvider,
               private photoViewer: PhotoViewer) {
+  }
+
+  ionViewDidLoad() {
+    console.log(this.navParams.get('mediaID'));
+    this.mediaProvider.getSingleMedia(this.navParams.get('mediaID')).subscribe(response => {
+      console.log(response);
+      this.url = this.mediaProvider.mediaUrl + response['filename'];
+      this.title = response['title'];
+      this.userid = response['user_id'];
+      this.file_id = response['file_id'];
+      this.description = response['description'];
+      const userToken = this.mediaProvider.userHasToken();
+
+      this.mediaProvider.getUserDataViaId(userToken, this.userid.toString()).subscribe((result: User) => {
+        console.log(result);
+        this.username = result['username'];
+        this.mediaProvider.getAllProfilePics().subscribe(data => {
+          console.log(data);
+          this.ppArray = data;
+          this.getLikes(this.file_id);
+        });
+
+      });
+
+    });
   }
 
   showImage() {
@@ -63,74 +92,41 @@ export class SinglePage {
 
   getLikes(id: number) {
     this.mediaProvider.getListOfLikes(id).subscribe(data => {
+      console.log(data);
       this.likesSet = data;
       this.amountOfLikes = Object.keys(data).length;
+      this.getComments(this.file_id);
     });
   }
 
   getComments(id: number) {
     this.mediaProvider.getCommentsFile(id).subscribe(data => {
+      console.log(data);
       this.commentsArray = data;
       this.amountOfComments = Object.keys(data).length;
     });
   }
 
-
-  ionViewDidLoad() {
-    console.log(this.navParams.get('mediaID'));
-    this.mediaProvider.getSingleMedia(this.navParams.get('mediaID')).subscribe(response => {
-      console.log(response);
-      this.url = this.mediaProvider.mediaUrl + response['filename'];
-      this.title = response['title'];
-      this.userid = response['user_id'];
-      this.file_id = response['file_id'];
-      const userToken = this.mediaProvider.userHasToken();
-
-
-      this.mediaProvider.getUserDataViaId(userToken, this.userid.toString()).subscribe((result: User) => {
-        console.log(result);
-        this.username = result['username'];
-        this.getLikes(this.file_id);
-        this.getComments(this.file_id);
-      });
-
-
-      console.log(typeof this.userid);
-      /* this.mediaProvider.getSingleMedia(this.navParams.get('mediaID')).
-          subscribe(response => {
-            console.log(response);
-            this.url = this.mediaProvider.mediaUrl + response['filename'];
-            this.title = response['title'];
-            this.mediaProvider.getTagByFile(response['file_id']).
-                subscribe(response => {
-                  console.log(response);
-                  if (response.length === 0) this.message = 'No tags';
-                  response.forEach(t => {
-                    const tag = JSON.parse(t['tag']);
-                    console.log(tag);
-                    if (tag.name === 'latLon') {
-                      this.latLon = tag.value;
-                    } else {
-                      return;
-                    }
-                    console.log(this.latLon);
-                    if (this.latLon === undefined) {
-                      this.message = 'No EXIF data';
-                    } else {
-                      this.mapProvider.loadMap('map_canvas', this.latLon);
-                    }
-                  });
-
-                });
-          }, (error: HttpErrorResponse) => {
-            console.log(error);
-          });*/
-
-      this.description = response['description'];
-
-    }, (error: HttpErrorResponse) => {
-      console.log(error);
-    });
+  getProfilePic(id: number) {
+    this.ownPicArray = this.ppArray.filter(comment => comment.user_id == id);
+    this.newestPicIndex = Object.keys(this.ownPicArray).length - 1;
+    if (Object.keys(this.ownPicArray).length > 0) {
+      this.profilePicUrl = this.mediaProvider.mediaUrl + this.ownPicArray[this.newestPicIndex].filename;
+      return this.profilePicUrl;
+    }
   }
+
+  getUsername(id: number) {
+    this.mediaProvider.getUserDataViaId(localStorage.getItem('token'), id).subscribe(data => {
+      this.commenter = data;
+      console.log(this.commenter);
+      console.log(this.commenter[0]);
+      console.log(this.commenter[0].username);
+
+      return this.commenter[0].username;
+    })
+  }
+
+
 
 }
