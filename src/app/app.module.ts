4a10c6b2eda15128f8903e4ec75ade784fc9d12f
eerokar <karvonen.eero@gmail.com>
2018-03-06 18:54:27 +0200

    getUserProfile now directs to own profile page when clicking own profile and everything works!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,78 +1,80 @@
-import {BrowserModule} from '@angular/platform-browser';	
-import {ErrorHandler, NgModule} from '@angular/core';	
-import {IonicApp, IonicErrorHandler, IonicModule} from 'ionic-angular';	
-import {SplashScreen} from '@ionic-native/splash-screen';	
-import {StatusBar} from '@ionic-native/status-bar';	
-	
-import {MyApp} from './app.component';	
-import {HttpClientModule} from '@angular/common/http';	
-import {MediaProvider} from '../providers/media/media';	
-import {LoginPage} from '../pages/login/login';	
-import {FrontPage} from '../pages/front/front';	
-import {UploadPage} from '../pages/upload/upload';	
-import {RegisterPage} from '../pages/register/register';	
-import {LogoutPage} from '../pages/logout/logout';	
-import {ProfilePage} from '../pages/profile/profile';	
-import {SinglePage} from '../pages/single/single';	
-import {FormsModule} from '@angular/forms';	
-import {PipesModule} from '../pipes/pipes.module';	
-import {PhotoViewer} from '@ionic-native/photo-viewer';	
-import {Camera} from '@ionic-native/camera';	
-import {Geolocation} from '@ionic-native/geolocation';	
-import {File} from '@ionic-native/file';	
-import {MapProvider} from '../providers/map/map';	
-import {EditorProvider} from '../providers/editor/editor';	
-import {TabsPageModule} from "../pages/tabs/tabs.module";	
-import {TabsPage} from "../pages/tabs/tabs";	
-import {UploadPpPage} from "../pages/upload-pp/upload-pp";	
-import {UploadPpPageModule} from "../pages/upload-pp/upload-pp.module";	
-	
-@NgModule({	
-  declarations: [	
-    MyApp,	
-    LoginPage,	
-    FrontPage,	
-    UploadPage,	
-    RegisterPage,	
-    LogoutPage,	
-    ProfilePage,	
-    SinglePage,	
-	
-  ],	
-  imports: [	
-    BrowserModule,	
-    IonicModule.forRoot(MyApp),	
-    FormsModule,	
-    PipesModule,	
-    HttpClientModule,	
-    TabsPageModule,	
-    UploadPpPageModule	
-  ],	
-  bootstrap: [IonicApp],	
-  entryComponents: [	
-    MyApp,	
-    LoginPage,	
-    FrontPage,	
-    UploadPage,	
-    RegisterPage,	
-    LogoutPage,	
-    ProfilePage,	
-    SinglePage,	
-    TabsPage,	
-    UploadPpPage	
-  ],	
-  providers: [	
-    StatusBar,	
-    SplashScreen,	
-    {provide: ErrorHandler, useClass: IonicErrorHandler},	
-    MediaProvider,	
-    PhotoViewer,	
-    Camera,	
-    Geolocation,	
-    File,	
-    MapProvider,	
-    EditorProvider,	
-  ],	
-})	
-export class AppModule {	
-}	
+import {BrowserModule} from '@angular/platform-browser';
+import {ErrorHandler, NgModule} from '@angular/core';
+import {IonicApp, IonicErrorHandler, IonicModule} from 'ionic-angular';
+import {SplashScreen} from '@ionic-native/splash-screen';
+import {StatusBar} from '@ionic-native/status-bar';
+
+import {MyApp} from './app.component';
+import {HttpClientModule} from '@angular/common/http';
+import {MediaProvider} from '../providers/media/media';
+import {LoginPage} from '../pages/login/login';
+import {FrontPage} from '../pages/front/front';
+import {UploadPage} from '../pages/upload/upload';
+import {RegisterPage} from '../pages/register/register';
+import {LogoutPage} from '../pages/logout/logout';
+import {ProfilePage} from '../pages/profile/profile';
+import {SinglePage} from '../pages/single/single';
+import {FormsModule} from '@angular/forms';
+import {PipesModule} from '../pipes/pipes.module';
+import {PhotoViewer} from '@ionic-native/photo-viewer';
+import {Camera} from '@ionic-native/camera';
+import {Geolocation} from '@ionic-native/geolocation';
+import {File} from '@ionic-native/file';
+import {MapProvider} from '../providers/map/map';
+import {EditorProvider} from '../providers/editor/editor';
+import {TabsPageModule} from "../pages/tabs/tabs.module";
+import {TabsPage} from "../pages/tabs/tabs";
+import {UploadPpPage} from "../pages/upload-pp/upload-pp";
+import {UploadPpPageModule} from "../pages/upload-pp/upload-pp.module";
+import {ViewProfilePageModule} from "../pages/view-profile/view-profile.module";
+
+@NgModule({
+  declarations: [
+    MyApp,
+    LoginPage,
+    FrontPage,
+    UploadPage,
+    RegisterPage,
+    LogoutPage,
+    ProfilePage,
+    SinglePage,
+
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+    FormsModule,
+    PipesModule,
+    HttpClientModule,
+    TabsPageModule,
+    UploadPpPageModule,
+    ViewProfilePageModule
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    LoginPage,
+    FrontPage,
+    UploadPage,
+    RegisterPage,
+    LogoutPage,
+    ProfilePage,
+    SinglePage,
+    TabsPage,
+    UploadPpPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    MediaProvider,
+    PhotoViewer,
+    Camera,
+    Geolocation,
+    File,
+    MapProvider,
+    EditorProvider,
+  ],
+})
+export class AppModule {
+}
--- src/pages/view-profile/view-profile.module.ts
+++ src/pages/view-profile/view-profile.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { ViewProfilePage } from './view-profile';
+import {PipesModule} from "../../pipes/pipes.module";
 
 @NgModule({
   declarations: [
@@ -8,6 +9,7 @@ import { ViewProfilePage } from './view-profile';
   ],
   imports: [
     IonicPageModule.forChild(ViewProfilePage),
+    PipesModule
   ],
 })
 export class ViewProfilePageModule {}
